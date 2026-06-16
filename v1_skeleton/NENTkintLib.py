80dfc34f920a76ea5d65fc680a208ace38af0a83
NenNinja <Ngaru.Ngawaka@gmail.com>
2026-06-16 12:19:48 +1200

    added grid display

--- v1_skeleton/NENTkintLib.py
+++ v1_skeleton/NENTkintLib.py
@@ -1,4 +1,6 @@
+from logging import root
 import tkinter as tk
+from tkinter import ttk
 from PIL import Image, ImageTk
 # mouse class (currently only tracks position) NOTE: from old program for now ignore
 class Mouse:
@@ -14,52 +16,136 @@ mouse = Mouse()
 # NOTE: Lazy man code
 # commonly used functions and variables are stored here to avoid making main.py messy
 
-def frame(stage=None, bg="#ffffff", column=0, row=0, columnspan=1, rowspan=1, sticky="", columnNum=None, rowNum=None):
-    frame = tk.Frame(stage, bg=bg)
-    frame.grid(column=column, row=row, columnspan=columnspan, rowspan=rowspan, sticky=sticky)
-    if columnNum is not None: #NOTE: use if here bc if you set columnNum to 0 it will skip the for i in range() since range(0) = just skips
+def frame(stage=None, bg="#ffffff", column=0, row=0, columnspan=1, rowspan=1, sticky="", columnNum=0, rowNum=0, showGrid=False, highLightColor="black", highLightWidth=0):
+    frame1 = tk.Frame(
+        stage, 
+        bg=bg, 
+        highlightbackground=highLightColor, 
+        highlightthickness=highLightWidth 
+        )
+    frame1.grid(
+        column=column, 
+        row=row, 
+        columnspan=columnspan, 
+        rowspan=rowspan, 
+        sticky=sticky
+        )
+    if columnNum != 0: #NOTE: use if here bc if you set columnNum to 0 it will skip the for i in range() since range(0) = just skips
         for i in range(columnNum): 
-            frame.columnconfigure(i, weight=1)
+            frame1.columnconfigure(i, weight=1)
     else:
-        frame.columnconfigure(0, weight=1)
-    if rowNum is not None:
+        frame1.columnconfigure(0, weight=1)
+    if rowNum != 0:
         for i in range(rowNum):
-            frame.rowconfigure(i, weight=1)
+            frame1.rowconfigure(i, weight=1)
     else:
-        frame.rowconfigure(0, weight=1)
-    return frame # return frame so you can use it to place other widgets inside it
+        frame1.rowconfigure(0, weight=1)
+    
+    if showGrid == True: # This grid is for debugging purposes, idea credit goes to Jaco (he inspired the idea for the grid)
+        for ix in range(columnNum) if columnNum != 0 else range(1):
+            print(ix)
+            for iy in range(rowNum) if rowNum != 0 else range(1):
+                print(iy)
+                frame(
+                    stage=frame1,
+                    column=ix, row=iy, 
+                    sticky="nsew", 
+                    bg="#ffffff",
+                    highLightColor="black",   # This sets the border color
+                    highLightWidth=1,        # This sets the border width in pixels
+                    )
+                
+    return frame1 # return frame so you can use it to place other widgets inside it
     # NOTE: eg. headerFrame = frame(yadda yadda)
     # NOTE: con. drawText(stage=headerFrame, yadda yadda) instead of drawText(stage=root, yadda yadda) (changed root to self remember >:[ )
 
 
 def drawText(stage=None, text="", bg="white", fg="black", fstyle="Arial", fsize=14, extra="normal", padx=0, pady=0, sticky="", column=0, row=0, columnspan=1, rowspan=1):
-    label = tk.Label(stage, text=text, bg=bg, fg=fg, font=(fstyle, fsize, extra), padx=padx, pady=pady)
-    label.grid(sticky=sticky, column=column, row=row, columnspan=columnspan, rowspan=rowspan)
+    label = tk.Label(
+        stage, 
+        text=text, 
+        bg=bg, 
+        fg=fg, 
+        font=(fstyle, fsize, extra), 
+        padx=padx, pady=pady
+        ).grid(
+            sticky=sticky, 
+            column=column, row=row, 
+            columnspan=columnspan, rowspan=rowspan
+            )
     return label # return label so you can use it to change the text later (eg. label.config(text="new text")) (for dynamic text like score, timer, etc.)
 
 def functionButton(stage=None, text="", bg="white", fg="black", fstyle="Arial", fsize=14, extra="normal", padx=0, pady=0, sticky="", command=None):
-    button = tk.Button(stage, text=text, bg=bg, fg=fg, font=(fstyle, fsize, extra), padx=padx, pady=pady, command=lambda: command)
-    button.grid(sticky=sticky)
+    button = tk.Button(
+        stage, 
+        text=text, 
+        bg=bg, 
+        fg=fg, 
+        font=(fstyle, fsize, extra), 
+        padx=padx, pady=pady, 
+        command=lambda: command
+        ).grid(
+            sticky=sticky
+            )
     return button
 
 def linkButton(stage=None, text="", bg="white", fg="black", fstyle="Arial", fsize=14, extra="normal", padx=0, pady=0, sticky="", controller=None, page=None, column=0, row=0, columnspan=1, rowspan=1):
-    button = tk.Button(stage, text=text, bg=bg, fg=fg, font=(fstyle, fsize, extra), padx=padx, pady=pady, command=lambda: controller.show_frame(page))
-    button.grid(sticky=sticky, column=column, row=row, columnspan=columnspan, rowspan=rowspan,)
+    button = tk.Button(
+        stage, 
+        text=text, 
+        bg=bg, 
+        fg=fg, 
+        font=(fstyle, fsize, extra), 
+        padx=padx, pady=pady, 
+        command=lambda: controller.show_frame(page)
+        ).grid(
+            sticky=sticky, 
+            column=column, 
+            row=row, 
+            columnspan=columnspan, 
+            rowspan=rowspan
+            )
     return button
 
 def drawEntry(stage=None, bg="white", fg="black", fstyle="Arial", fsize=14, extra="normal", padx=0, pady=0, sticky="", column=0, row=0, columnspan=1, rowspan=1):
-    entry = tk.Entry(stage, bg=bg, fg=fg, font=(fstyle, fsize, extra))
-    entry.grid(sticky=sticky, column=column, row=row, columnspan=columnspan, rowspan=rowspan, padx=padx, pady=pady)
+    entry = tk.Entry(
+        stage, 
+        bg=bg, 
+        fg=fg, 
+        font=(fstyle, fsize, extra)
+        ).grid(
+            sticky=sticky, 
+            column=column, 
+            row=row, 
+            columnspan=columnspan, 
+            rowspan=rowspan, 
+            padx=padx, 
+            pady=pady
+            )
     return entry
+
+def radioButton(stage=None, text="", variable=None, value=None, bg="white", fg="black", fstyle="Arial", fsize=14, extra="normal", padx=0, pady=0, sticky="", column=0, row=0, columnspan=1, rowspan=1):
+    radio = ttk.Radiobutton(
+        stage, 
+        text=text, 
+        variable=variable, 
+        value=value
+        ).grid(
+            sticky=sticky, 
+            column=column, 
+            row=row, 
+            columnspan=columnspan, 
+            rowspan=rowspan, 
+            padx=padx, pady=pady
+            )
+    return radio
 
 def image(stage=None, fileDIR=None, size=[100,100], sticky=""):
         try:
-            pil_image = Image.open(fileDIR)
-            resized_image = pil_image.resize((size[0], size[1]), Image.Resampling.LANCZOS) # NOTE: .Resampling.LANCZOS allows resizing/changing pixel ratio
-            tk_image = ImageTk.PhotoImage(resized_image)
-            image_label = tk.Label(stage, image=tk_image)
-            image_label.grid(sticky=sticky)
-            image_label.image = tk_image
-            return image_label
+            resizedImage = Image.open(fileDIR).resize((size[0], size[1]), Image.Resampling.LANCZOS) # NOTE: .Resampling.LANCZOS allows resizing/changing pixel ratio
+            tkImage = ImageTk.PhotoImage(resizedImage)
+            imageLabel = tk.Label(stage, image=tkImage).grid(sticky=sticky)
+            imageLabel.image = tkImage
+            return imageLabel
         except:
              print("err: please check file directory and stage")
--- v1_skeleton/main.py
+++ v1_skeleton/main.py
@@ -12,7 +12,7 @@ W, H = 400, 400
 
 # WARNING! this version of the program contains many of my personal notes
 # for a more proffesional and easy to read program please see other folder (NOTE: chat... make other version with less notes)
-# any comment including NOTE: is a personal note and can be ignored, but it may be helpful to read it if you want to understand the code better :3
+# any comment including NOTE: is a personal note and can be ignored, but it may be helpful to read it if you want to understand the code better
 
 # HUGE DISCOVERY
 # by passing tk.tk through the main class (making MAIN a child/inheriter of tk.tk) MAIN inherits all tk.tk 's functions, variables, EVERYTHING
@@ -23,6 +23,8 @@ W, H = 400, 400
 backgroundColor1 = "#bdbdbd"
 headerColor1 = "#a0a0a0"
 buttonColor1 = "#ffffff"
+
+showGrids = False
 
 
 class MAIN(tk.Tk):
@@ -72,23 +74,30 @@ class HomePage(tk.Frame):
         self.columnconfigure(1,weight=1)
         self.rowconfigure(0, weight=3)
         self.rowconfigure((1, 2),weight=5)
+        
 
         # header frame
-        headerFrame = frame(self, headerColor1, column=0, row=0, columnspan=3, sticky="nsew", columnNum=10)
+        headerFrame = frame(self, headerColor1, column=0, row=0, columnspan=3, sticky="nsew", columnNum=10, showGrid=showGrids)
+        
         drawText(headerFrame, "PFP [placeholder]", bg="#ffffff", column=0, row=0, sticky="nsew", fsize=10)
         drawText(headerFrame, "Username", bg=headerColor1, column=1, row=0, sticky="nsew", fsize=30)
         drawText(headerFrame, "Search:", bg=headerColor1, column=9, row=0, sticky="e", fsize=20, padx=10)
         drawEntry(headerFrame, bg="#ffffff", column=10, padx=(0,30), sticky="w")
 
         # buttons frame
-        buttonFrame = frame(self, bg=backgroundColor1, column=1, row=1, rowspan=2, sticky="nsew", rowNum=10)
+        buttonFrame = frame(self, bg=backgroundColor1, column=1, row=1, rowspan=2, sticky="nsew", rowNum=10, showGrid=showGrids)
         linkButton(buttonFrame, bg=buttonColor1, text="Blog", page="PlaceHolderPage", controller=controller, column=0, row=0, sticky="news", fsize=30)
         linkButton(buttonFrame, bg=buttonColor1, text="Courses", page="PlaceHolderPage", controller=controller, column=0, row=1, sticky="news", fsize=30)
         linkButton(buttonFrame, bg=buttonColor1, text="Contact Us", page="PlaceHolderPage", controller=controller, column=0, row=10, sticky="news", fsize=30)
+        variable = ""
+        radioButton(buttonFrame, text="Dark Mode", variable=variable, value="dark", bg=backgroundColor1, fg="black", fstyle="Arial",  sticky="news", column=0, row=9)
+        radioButton(buttonFrame, text="Light Mode", variable=variable, value="light", bg=backgroundColor1, fg="black", fstyle="Arial", sticky="news", column=0, row=8)
+        
 
         # settings frame (placing)
-        settingsFrame = frame(self, bg=backgroundColor1, column=2, row=1, rowspan=2, sticky="nsew", columnNum=4, rowNum=10)
+        settingsFrame = frame(self, bg=backgroundColor1, column=2, row=1, rowspan=2, sticky="nsew", columnNum=4, rowNum=10, showGrid=showGrids)
         linkButton(settingsFrame, bg=buttonColor1, text="settings", page="PlaceHolderPage", controller=controller, column=4, row=10, sticky="news", fsize=30)
+        
         #NOTE: Need to replace the settings text with a gear icon, but for now this will do :3
         
 
